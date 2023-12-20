48107cd965fbd6bcdba9ffad6e8acd70954027f5
Henshal <henshal@Henshals-MacBook-Air.local>
2023-12-20 16:02:08 +0400

    readme: Added readme file

--- app/utils/initial.js
+++ app/utils/initial.js
@@ -1,10 +1,10 @@
 import Cookies from "js-cookie";
 
 const initialState = {
-  host: Cookies.get("host") || "",
-  database: Cookies.get("database") || "",
-  username: Cookies.get("username") || "",
-  password: Cookies.get("password") || "",
+  host: Cookies.get("host") || "65817b57.dorsy.net",
+  database: Cookies.get("database") || "d03f016a",
+  username: Cookies.get("username") || "d03f016a",
+  password: Cookies.get("password") || "Nvus3JkuHYzYDfGFPk6V",
   connected: false,
   query: "SELECT * FROM `Daily_Email_Log`",
   graphTitle: ""
