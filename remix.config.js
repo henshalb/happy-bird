638ccae953c2fdf91fe7ea8c5d43d06cf5a1948d
Henshal <henshal@Henshals-MacBook-Air.local>
2023-12-20 18:37:01 +0400

    readme: Added readme file

--- remix.config.js
+++ remix.config.js
@@ -1,23 +1,23 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   postcss: true,
-  serverModuleFormat: 'cjs',
+  // serverModuleFormat: 'cjs',
   ignoredRouteFiles: ["**/.*"],
-  browserNodeBuiltinsPolyfill: {
-    modules: {
-      crypto: true,
-      events: true,
-      net: true,
-      tls: true,
-      url: true,
-      buffer: true,
-      util: true,
-      timers: true,
-      fs: true,
-      stream: true,
-      string_decoder: true,
-      process: true,
-      zlib: true,
-    },
-  },
+  // browserNodeBuiltinsPolyfill: {
+  //   modules: {
+  //     crypto: true,
+  //     events: true,
+  //     net: true,
+  //     tls: true,
+  //     url: true,
+  //     buffer: true,
+  //     util: true,
+  //     timers: true,
+  //     fs: true,
+  //     stream: true,
+  //     string_decoder: true,
+  //     process: true,
+  //     zlib: true,
+  //   },
+  // },
 };
